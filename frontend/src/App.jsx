a15606b8d555c8ec46f7e7a32ad75e20873d3aee
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2025-12-19 23:24:00 +0000

    added tabs for other features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import { useEffect, useState } from 'react';
 
 
 function App() {
-
   const [summaries, setSummaries] = useState([]);
   const [stocks, setStocks] = useState([]);
+  const [selectedTab, setSelectedTab] = useState('Summaries')
 
   useEffect(() => {
     async function loadData() {
@@ -27,21 +27,45 @@ function App() {
     <div className='header'>
       <h2>My Stock Tracker</h2>
     </div>
-      <h1>Stock Summaries</h1>
-      <div className='summary-list'>
-        {summaries.map((summary, index) => (
-          <Summary 
-            key={index}
-            company={summary.company} 
-            ticker={summary.ticker} 
-            title={summary.title} 
-            body={summary.body} 
-            sentiment={summary.sentiment} 
-            key_points={summary.key_points}
-            />
-        ))}
-
-      </div>
+    <div className='tabs'>
+      <span 
+        className={selectedTab == 'Summaries' ? 'selected' : ''}
+        onClick={() => setSelectedTab('Summaries')}
+      >Summaries</span>
+      <span
+        className={selectedTab == 'Calendar' ? 'selected' : ''}
+        onClick={() => setSelectedTab('Calendar')}
+      >Calendar</span>
+      <span
+        className={selectedTab == 'Stocks' ? 'selected' : ''}
+        onClick={() => setSelectedTab('Stocks')}
+      >Stocks</span>
+    </div>
+    <div 
+      hidden={selectedTab == 'Summaries' ? false : true}
+      className='summary-list'>
+      {summaries.map((summary, index) => (
+        <Summary 
+          key={index}
+          company={summary.company} 
+          ticker={summary.ticker} 
+          title={summary.title} 
+          body={summary.body} 
+          sentiment={summary.sentiment} 
+          key_points={summary.key_points}
+          />
+      ))}
+    </div>
+    <div 
+      hidden={selectedTab == 'Calendar' ? false : true}
+      className='calendar-container'>
+        Calendar
+    </div>
+    <div 
+      hidden={selectedTab == 'Stocks' ? false : true}
+      className='stocks-list'>
+        Stocks
+    </div>
     </>
   )
 }
